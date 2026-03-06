e0bab9ca0d9aecc143e5a5b2902565f196b3fb9e
fchau <fchau@hotmail.es>
2026-03-06 16:12:08 +0100

    Arreglos a Flask

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, url_for
+from sqlmodel import Session, select
+from database import engine, crear_db
+from models import Inscrito
+
+# Crear DB al arrancar
+crear_db()
+
+app = Flask(__name__)
+
+# --- RUTAS ---
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/evento")
+def evento():
+    return render_template("evento.html")
+
+@app.route("/galeria")
+def galeria():
+    return render_template("galeria.html")
+
+@app.route("/inscripcion")
+def inscripcion_view():
+    return render_template("inscripcion.html")
+
+@app.route("/inscribir", methods=["POST"])
+def inscribir():
+    nuevo = Inscrito(
+        nombre=request.form["nombre"],
+        email=request.form["email"],
+        telefono=request.form["telefono"],
+        asistencia=request.form["asistencia"],
+        mensaje=request.form.get("mensaje")
+    )
+    with Session(engine) as session:
+        session.add(nuevo)
+        session.commit()
+    return redirect(url_for('inscripcion_view', msg='gracias'))
+
+@app.route("/admin")
+def admin():
+    with Session(engine) as session:
+        inscritos = session.exec(select(Inscrito)).all()
+    return render_template("admin.html", inscritos=inscritos)
+
+if __name__ == "__main__":
+    app.run()
