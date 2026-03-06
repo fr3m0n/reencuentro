95c89610dd130ceb630a771bd5351362ed718ce8
fchau <fchau@hotmail.es>
2026-03-06 11:53:11 +0100

    Arreglos finales: DB segura y CSS limpio

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlmodel import SQLModel, create_engine
 import os
 
-# Detecta la carpeta actual para guardar la base de datos ahí
+# Esta línea encuentra la carpeta real donde está tu proyecto
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 database_path = os.path.join(BASE_DIR, "database.db")
 
-# Usamos la ruta absoluta
+# Usamos la ruta segura
 engine = create_engine(f"sqlite:///{database_path}")
 
 def crear_db():
