431b5df64bc384b9f9b953b0d87e9d4fbaa08836
fchau <fchau@hotmail.es>
2026-03-06 11:33:52 +0100

    Mejoras para despliegue en Render

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
 from sqlmodel import SQLModel, create_engine
+import os
 
-engine = create_engine("sqlite:///database.db")
+# Detecta la carpeta actual para guardar la base de datos ahí
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+database_path = os.path.join(BASE_DIR, "database.db")
+
+# Usamos la ruta absoluta
+engine = create_engine(f"sqlite:///{database_path}")
 
 def crear_db():
     SQLModel.metadata.create_all(engine)
